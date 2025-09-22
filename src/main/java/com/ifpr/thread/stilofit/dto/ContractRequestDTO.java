bc6bac87231ff6f2f138fd5105cbf246b28ebba0
Vinicius-Oliveira-R <viniciusoliveira12353@gmail.com>
2025-09-22 20:18:09 -0300

    feat(Back-End): Cria o Contrato request

---
+++ src/main/java/com/ifpr/thread/stilofit/dto/ContractRequestDTO.java
@@ -0,0 +1,40 @@
+package com.ifpr.thread.stilofit.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ContractRequestDTO {
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String status;
+
+    @NotBlank
+    private String template;
+
+    @NotBlank
+    private String installmentable;
+
+    private Integer installments;
+
+    @NotNull
+    private Double totalValue;
+
+    private Double installmentsValue;
+
+    @NotNull
+    private Integer expire;
+
+    @NotBlank
+    private String typeExpire;
+
+    private List<String> classRoms;
+    private String timeMin;
+    private String timeMax;
+    private List<String> weekdays;
+}
