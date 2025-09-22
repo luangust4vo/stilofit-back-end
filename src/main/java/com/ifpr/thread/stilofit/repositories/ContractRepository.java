241df4a4add6e0ff0825ac279ebd35005dceebad
Vinicius-Oliveira-R <viniciusoliveira12353@gmail.com>
2025-09-22 20:46:43 -0300

    feat(Back-End): Cria a classe repository para contrato

---
+++ src/main/java/com/ifpr/thread/stilofit/repositories/ContractRepository.java
@@ -0,0 +1,7 @@
+package com.ifpr.thread.stilofit.repositories;
+
+import com.ifpr.thread.stilofit.models.Contract;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContractRepository extends JpaRepository<Contract, Long> {
+}
