91ff928bfbdaa5ed1711005376560d7bd0b2d1eb
ThallesSorrilha <thallessorrilha@gmail.com>
2025-10-02 15:47:42 -0300

    feature(repository/Sale): adicionando classe de repositório para venda, com consulta pelo cliente

---
+++ src/main/java/com/ifpr/thread/stilofit/repositories/SaleRepository.java
@@ -0,0 +1,15 @@
+package com.ifpr.thread.stilofit.repositories;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.ifpr.thread.stilofit.models.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    @Query("SELECT s FROM Sale s WHERE s.clientId = :clientId")
+    Page<Sale> findByClientId(@Param("clientId") int clientId, Pageable pageable);
+}
