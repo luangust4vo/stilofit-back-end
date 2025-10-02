efd9a99d7a6c451bf6ab03006aa65c5c25e71355
ThallesSorrilha <thallessorrilha@gmail.com>
2025-10-02 14:42:32 -0300

    feature(model/Sale): adicionando classe de modelo para vendas

--- src/main/java/com/ifpr/thread/stilofit/models/Sale.java
+++ src/main/java/com/ifpr/thread/stilofit/models/Sale.java
@@ -1,12 +1,18 @@
 package com.ifpr.thread.stilofit.models;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -17,12 +23,26 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "sale")
+@Table(name = "sales")
 public class Sale {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "client_id")
+    @ManyToOne
+    @JoinColumn(name = "id_clients")
+    @NotNull(message = "{validation.ClientID.notnull}")
+    private int clientId;
+
+    @Column(name = "contracts_ids")
+    @OneToMany(mappedBy = "sales", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<Contract> contractsIds;
+
+    @Column(name = "totalAmount")
+    @NotNull(message = "{validation.totalAmount.notnull}")
+    private double totalAmount;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     @Temporal(TemporalType.TIMESTAMP)
@@ -31,10 +51,6 @@ public class Sale {
     @Column(name = "updated_at", nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime updatedAt;
-
-    @Column(name = "totalAmount")
-    @NotNull(message = "{validation.totalAmount.notnull}")
-    private double totalAmount;
 
     @PrePersist
     protected void onCreate() {
