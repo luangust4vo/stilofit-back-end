9d10651686bb916c958efea10ab2ffef94b5eff1
larybino <binolaryssa@gmail.com>
2025-10-11 13:29:31 -0300

    feat: adiciona dtos de funcionarios

---
+++ src/main/java/com/ifpr/thread/stilofit/dto/EmployeeRequestDTO.java
@@ -0,0 +1,55 @@
+package com.ifpr.thread.stilofit.dto;
+
+import java.io.ObjectInputFilter.Status;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javax.management.relation.Role;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.ifpr.thread.stilofit.models.enums.Gender;
+import com.ifpr.thread.stilofit.models.enums.MaritalStatus;
+import com.ifpr.thread.stilofit.models.enums.Shift;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class EmployeeRequestDTO {
+    @NotBlank(message = "{validation.name.notblank}")
+    private String name;
+    @Email
+    private String email;
+    private String password;
+    @NotNull(message = "{validation.birth.notblank}")
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate birthDate;
+    @NotNull(message = "{validation.gender.notblank}")
+    private Gender gender;
+    private MaritalStatus maritalStatus;
+    @NotBlank(message = "{validation.cpf.notblank}")
+    @Size(max = 14)
+    @CPF(message = "{validation.cpf.valid}")
+    private String cpf;
+    private String rg;
+    private String professionalRegister;
+    private String guardianPhone;
+    private String cellphone;
+    private Role role;
+    private Status status;
+    private String cep;
+    private String address;
+    private String district;
+    private String city;
+    private String state;
+    private String number;
+    private String complement;
+    private Shift shift;
+    private LocalTime timeMin;
+    private LocalTime timeMax;
+}
---
+++ src/main/java/com/ifpr/thread/stilofit/dto/EmployeeResponseDTO.java
@@ -0,0 +1,41 @@
+package com.ifpr.thread.stilofit.dto;
+
+import java.io.ObjectInputFilter.Status;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import javax.management.relation.Role;
+
+import com.ifpr.thread.stilofit.models.enums.Gender;
+import com.ifpr.thread.stilofit.models.enums.MaritalStatus;
+import com.ifpr.thread.stilofit.models.enums.Shift;
+
+import lombok.Data;
+
+@Data
+public class EmployeeResponseDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDate birthDate;
+    private Gender gender;
+    private MaritalStatus maritalStatus;
+    private String cpf;
+    private String rg;
+    private String professionalRegister;
+    private String guardianPhone;
+    private String cellphone;
+    private Role role;
+    private Status status;
+    private String cep;
+    private String address;
+    private String district;
+    private String city;
+    private String state;
+    private String number;
+    private String complement;
+    private Shift shift;
+    private LocalTime timeMin;
+    private LocalTime timeMax;
+}
