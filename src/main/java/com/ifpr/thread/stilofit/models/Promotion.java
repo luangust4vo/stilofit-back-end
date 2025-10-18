f61c6072211ad6b773536fb9820581ea719856e4
ThallesSorrilha <thallessorrilha@gmail.com>
2025-10-17 22:26:04 -0300

    refactor(models/ Promotion & Discount): criando super classe de desconto, para que as classes de promoção e convênio herdem os atributos, simplificando Promotion

--- src/main/java/com/ifpr/thread/stilofit/models/Promotion.java
+++ src/main/java/com/ifpr/thread/stilofit/models/Promotion.java
@@ -1,48 +1,21 @@
 package com.ifpr.thread.stilofit.models;
 
-import com.ifpr.thread.stilofit.models.enums.DiscountType;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Entity
 @Table(name = "promotion")
-public class Promotion {
+@EqualsAndHashCode(callSuper = true)
+public class Promotion extends Discount {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
-    @NotBlank(message = "{validation.name.notblank}")
-    private String name;
-
-    @Column(name = "description")
-    private String description;
-
-    @Column(name = "discount_type")
-    @NotNull(message = "{validation.discountType.notnull}")
-    @Enumerated(EnumType.STRING)
-    private DiscountType discountType;
-
-    @Column(name = "value")
-    @NotNull(message = "{validation.value.notnull}")
-    @Positive(message = "{validation.value.positive}")
-    private Double value;
-
-    @Column(name = "times_applied")
-    @NotNull(message = "{validation.timesApplied.notnull}")
-    @Positive(message = "{validation.timesApplied.positive}")
-    private Integer timesApplied;
 }
