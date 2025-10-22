d3642f2c4332705eb971995849799278d2e024f5
ThallesSorrilha <thallessorrilha@gmail.com>
2025-10-22 17:08:51 -0300

    feature(PromotionRepository:findByName): adicionando o método para encontrar registros pelo nome

--- src/main/java/com/ifpr/thread/stilofit/repositories/PromotionRepository.java
+++ src/main/java/com/ifpr/thread/stilofit/repositories/PromotionRepository.java
@@ -4,11 +4,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.ifpr.thread.stilofit.models.Client;
 import com.ifpr.thread.stilofit.models.Promotion;
 
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
 
     @Query("SELECT p FROM Promotion p ORDER BY p.name ASC")
     Page<Promotion> findAll(Pageable pageable);
+
+    @Query("SELECT p FROM Promotion p WHERE LOWER(p.name) LIKE CONCAT('%', LOWER(:name), '%') ORDER BY p.name ASC")
+    Page<Client> findByName(Pageable pageable, @Param("name") String name);
 }
