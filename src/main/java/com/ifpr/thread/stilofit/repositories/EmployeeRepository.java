efa5b5c40f03863faf4aa4e465cee1188d655807
larybino <binolaryssa@gmail.com>
2025-10-21 21:45:33 -0300

    feat: Adiciona m´todo de listagem em service

--- src/main/java/com/ifpr/thread/stilofit/repositories/EmployeeRepository.java
+++ src/main/java/com/ifpr/thread/stilofit/repositories/EmployeeRepository.java
@@ -1,6 +1,9 @@
 package com.ifpr.thread.stilofit.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.ifpr.thread.stilofit.models.Employee;
 
@@ -8,4 +11,7 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     boolean existsByCpf(String cpf);
 
     boolean existsByProfessionalRegister(String professionalRegister);
+
+    @Query("SELECT e FROM Employee e ORDER BY e.name ASC")
+    Page<Employee> findAll(Pageable pageable);
 }
