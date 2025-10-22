b3a97e38b0118ca081d9136e0a3961e7c671374d
ThallesSorrilha <thallessorrilha@gmail.com>
2025-10-22 19:02:44 -0300

    fix(PromotionController): corrigindo nome de rotas para evitar conflito

--- src/main/java/com/ifpr/thread/stilofit/controllers/PromotionController.java
+++ src/main/java/com/ifpr/thread/stilofit/controllers/PromotionController.java
@@ -54,7 +54,7 @@ public class PromotionController {
     @Operation(summary = "Find Promotion by ID", description = "Retrieves a Promotion by their unique ID.", responses = {
         @ApiResponse(responseCode = "200", description = "Promotion found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = PromotionResponseDTO.class))),
         @ApiResponse(responseCode = "404", description = "Promotion not found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),})
-    @GetMapping("/{id}")
+    @GetMapping("/find-by-id/{id}")
     public ResponseEntity<PromotionResponseDTO> findById(@PathVariable Long id) {
         Promotion promotion = promotionService.findById(id);
         PromotionResponseDTO promotionResponse = PromotionMapper.toResponse(promotion);
@@ -66,7 +66,7 @@ public class PromotionController {
         @ApiResponse(responseCode = "404", description = "No promotions found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
         @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
     })
-    @GetMapping("/list-all-promotions")
+    @GetMapping("/list-all")
     public ResponseEntity<Page<PromotionListDTO>> findAll(@PageableDefault(size = 30, sort = "name", direction = Sort.Direction.ASC) Pageable pageable) {
         Page<Promotion> promotions = promotionService.findAll(pageable);
         Page<PromotionListDTO> promotionResponses = promotions.map(PromotionMapper::toList);
@@ -78,7 +78,7 @@ public class PromotionController {
         @ApiResponse(responseCode = "404", description = "No promotions found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
         @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
     })
-    @GetMapping("/list-promotions-by-name")
+    @GetMapping("/find-by-name")
     public ResponseEntity<Page<PromotionListDTO>> findByName(@PageableDefault(size = 30, sort = "name", direction = Sort.Direction.ASC) Pageable pageable, @RequestParam(name = "name", required = false, defaultValue = "") String name) {
         Page<Promotion> promotions = promotionService.findByName(pageable, name);
         Page<PromotionListDTO> promotionResponses = promotions.map(PromotionMapper::toList);
