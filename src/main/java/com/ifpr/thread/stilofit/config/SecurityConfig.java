5aa24c8bf29d7fc04386b01f3e7658c574d36e38
Luan Gustavo <88088757+luangust4vo@users.noreply.github.com>
2025-11-25 22:35:56 -0300

    Refactor SecurityConfig to remove unused import

--- src/main/java/com/ifpr/thread/stilofit/config/SecurityConfig.java
+++ src/main/java/com/ifpr/thread/stilofit/config/SecurityConfig.java
@@ -6,15 +6,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.Customizer;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
      public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors(Customizer.withDefaults())
-            .csrf(csrf -> csrf.disable()) 
+        http.csrf(csrf -> csrf.disable()) 
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
