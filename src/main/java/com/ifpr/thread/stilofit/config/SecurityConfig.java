09bc72e46b3ea9add5f1d62eb3c6ca353576f2e7
Luan Gustavo <88088757+luangust4vo@users.noreply.github.com>
2025-11-25 22:28:39 -0300

    Update SecurityConfig.java

--- src/main/java/com/ifpr/thread/stilofit/config/SecurityConfig.java
+++ src/main/java/com/ifpr/thread/stilofit/config/SecurityConfig.java
@@ -12,7 +12,8 @@ public class SecurityConfig {
 
     @Bean
      public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) 
+        http.cors(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable()) 
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
