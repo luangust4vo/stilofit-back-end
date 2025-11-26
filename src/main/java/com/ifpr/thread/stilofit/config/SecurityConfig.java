50f0a4b09e016c214088c52e850bb7fe7eedae30
Luan Gustavo <88088757+luangust4vo@users.noreply.github.com>
2025-11-25 22:39:07 -0300

    Update SecurityConfig to include CORS support

--- src/main/java/com/ifpr/thread/stilofit/config/SecurityConfig.java
+++ src/main/java/com/ifpr/thread/stilofit/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ifpr.thread.stilofit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,9 +12,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) 
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .cors(Customizer.withDefaults()) 
+            .csrf(csrf -> csrf.disable()) 
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        
         return http.build();
     }
 
@@ -21,5 +25,4 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
 }
